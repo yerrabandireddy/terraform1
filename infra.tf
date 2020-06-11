6a04dc0df486aada8bfff3cf9e32179a52cb60b9
yerrabandireddy <62086271+yerrabandireddy@users.noreply.github.com>
2020-06-11 16:06:45 +0530

    Update infra.tf

--- infra.tf
+++ infra.tf
@@ -1,1 +1,11 @@
-
+provider "aws"{
+    region = "ap-south-1"
+}
+resource "aws_instance" "testinstance"{
+    ami="${var.ami}"
+    instance_type = "${var.instance_type}"
+    key_name = "${var.key}"
+    tags = {
+        Name = "terra2"
+    }
+}
